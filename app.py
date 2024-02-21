e82e11e3fa3fdd193c9fbea4338001e13a025802
Autumniya <43678984+Autumniya@users.noreply.github.com>
2024-02-21 17:38:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -89,4 +89,6 @@ def list_subscribers_html():
     all_subscriptions = Subscription.query.filter(Subscription.is_active == True).all()
     return render_template('subscribers.html', subscriptions=all_subscriptions)
 
-initialize_db()
+if __name__ == '__main__':
+    initialize_db()
+    app.run(debug=True)
